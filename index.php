a190c971ec8cc12677d4e46953415b405da5557c
winston138 <vadimfumioka@mail.ru>
2023-11-24 00:49:34 +0800

    начало верстки

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");?>
 
-<p>Djida</p>
+
 
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
--- local/templates/book/header.php
+++ local/templates/book/header.php
@@ -1,1 +1,87 @@
-<p>header</p>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="stylesheet" href="/css/main.css">
+    <title>Document</title>
+</head>
+<body>
+
+
+<div class="header">
+    <div class="container">
+            <div class="logo">
+                <img src="/img/logo.jpg" alt="#">
+                <p>МБУ "Централизованная библиотечная система" мо "Джидинский район"</p>
+            </div>
+        <nav>
+            <nav class="header__menu">
+                <ul class="header__menu-list">
+                    <li>
+                        <a href="">Читателям</a>
+                        <ul>
+                            <li><a href="">Как стать читателем</a></li>
+                            <li><a href="">Услуги</li>
+                            <li><a href="">Виртуальные книжные выставки</a></li>
+                            <li><a href="">Новинки книг</a></li>
+                            <li><a href="">Анонсы мероприятий</a></li>
+                            <li><a href="">Вопрос-ответ</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="">Ресурсы</a>
+                        <ul>
+                            <li><a href="">Электронный каталог</a></li>
+                            <li><a href="">Газеты</li>
+                            <li><a href="">Базы данных</a></li>
+                            <li><a href="">Электронные библиотеки</a></li>
+                            <li><a href="">Наши разработки</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="">Краеведение</a>
+                        <ul>
+                            <li><a href="">Литературная карта</a></li>
+                            <li><a href="">Летопись  села</li>
+                            <li><a href="">Книги памяти</a></li>
+                            <li><a href="">История малой Родины в лицах</a></li>
+                            <li><a href="">Джидинский район от А до Я</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="">События</a>
+                        <ul>
+                            <li><a href="">Пушкинская карта</a></li>
+                            <li><a href="">Акции</li>
+                            <li><a href="">Мероприятия</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="">О библиотеке</a>
+                        <ul>
+                            <li><a href="">Структура библиотеки</a></li>
+                            <li><a href="">Документы</li>
+                            <li><a href="">Режим работы</a></li>
+                            <li><a href="">Контакты</a></li>
+                            <li><a href="">Отчет о деятельности</a></li>
+                            <li><a href="">Награды</a></li>
+                            <li><a href="">История</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </nav>
+        </nav>
+    </div>
+</div>
+
+
+
+
+
+
+</body>
+</html>
