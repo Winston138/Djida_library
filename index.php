b27e96363707ad83c1d5b7fafb8a1b73fae2a42d
mtheoz <theozzz@yandex.ru>
2023-12-01 02:06:24 +0800

    часть работ по .main__bg

--- index.php
+++ index.php
@@ -4,13 +4,6 @@
 <div class="container">
     <div class="main">
         <div class="main__bg">
-            <style>
-                .main__bg {
-                    background-image: url("/img/d1.png");
-                    background-repeat: no-repeat;
-                    background-position: top -120px right -120px;
-                }
-            </style>
             <div class="main__text">
                 По своему типу краеведение ближе к истории географии.
                 Но в отличие от последней, большре значение придает
@@ -29,31 +22,21 @@
             </div>
 
             <div class="main__chapter">
-                <div class="main__chapter-item">
-                    <a href="/local_history/literary_map/index.php" class="main__chapter-link">
-                        <div>ЛИТЕРАТУРНАЯ КАРТА</div>
-                    </a>
-                </div>
-                <div class="main__chapter-item">
-                    <a href="/local_history/chronicle/index.php" class="main__chapter-link">
-                        <div>ЛЕТОПИСЬ СЕЛА</div>
-                    </a>
-                </div>
-                <div class="main__chapter-item">
-                    <a href="/local_history/books_of_memory/index.php" class="main__chapter-link">
-                        <div>КНИГА ПАМЯТИ</div>
-                    </a>
-                </div>
-                <div class="main__chapter-item">
-                    <a href="/local_history/history_of_Motherland/index.php" class="main__chapter-link">
-                        <div>ИСТОРИЯ В ЛИЦАХ</div>
-                    </a>
-                </div>
-                <div class="main__chapter-item">
-                    <a href="/local_history/dzhidinsky_district/index.php" class="main__chapter-link">
-                        <div>ДЖИДИНСКИЙ РАЙОН</div>
-                    </a>
-                </div>
+	            <a href="/local_history/literary_map/" class="main__chapter-link d-block p-3 fw-bold">
+		            ЛИТЕРАТУРНАЯ КАРТА
+	            </a>
+	            <a href="/local_history/chronicle/" class="main__chapter-link d-block p-3 fw-bold">
+		            ЛЕТОПИСЬ СЕЛА
+	            </a>
+	            <a href="/local_history/books_of_memory/" class="main__chapter-link d-block p-3 fw-bold">
+		            КНИГА ПАМЯТИ
+	            </a>
+	            <a href="/local_history/history_of_Motherland/" class="main__chapter-link d-block p-3 fw-bold">
+		            ИСТОРИЯ В ЛИЦАХ
+	            </a>
+	            <a href="/local_history/dzhidinsky_district/" class="main__chapter-link d-block p-3 fw-bold">
+		            ДЖИДИНСКИЙ РАЙОН
+	            </a>
             </div>
         </div>
 
