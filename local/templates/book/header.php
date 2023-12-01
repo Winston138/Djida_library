2cc947df57d44c8bc8fecc593f2effad1f1f770d
mtheoz <theozzz@yandex.ru>
2023-12-02 03:31:03 +0800

    Работа над header

--- local/templates/book/header.php
+++ local/templates/book/header.php
@@ -10,7 +10,7 @@
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Bitter:wght@400;600;700&family=Ruda:wght@400;600;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Bitter:wght@400;600;700&family=Ruda:wght@400;500;600;700&display=swap" rel="stylesheet">
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
 
@@ -24,99 +24,89 @@
 
 
 
-<title>Document</title>
+	<title>Document</title>
 </head>
 <body>
-
     <header class="header">
         <div class="header__bg mb-4">
-            <style>
-                .header__bg {
-                    background-image: url("/img/bg.png");
-                    background-repeat: no-repeat;
-                    background-size: cover;
-                }
-            </style>
             <div class="container">
-                <nav class="navigation navbar navbar-expand-lg">
-                    <div class="container-fluid">
-                        <div class="logo">
-                            <a href="/" class="d-flex align-items-center">
-                                <img src="/img/logo.png" alt="#" class="d-block me-3">
-                            </a>
-                        </div>
-                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                            <span class="navbar-toggler-icon"></span>
-                        </button>
-                        <div class="header__menu-list collapse navbar-collapse" id="navbarSupportedContent">
-                            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                                <li class="nav-item dropdown">
-                                    <a class="nav-link" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                        Читателям
-                                    </a>
-                                    <ul class="dropdown-menu">
-                                        <li><a class="dropdown-item" href="/readers/become_a_reader/index.php">Как стать читателем</a></li>
-                                        <li><a class="dropdown-item" href="/readers/services/index.php">Услуги</a></li>
-                                        <li><a class="dropdown-item" href="/readers/virtual_book/index.php">Виртуальные книжные выставки</a></li>
-                                        <li><a class="dropdown-item" href="/readers/new_books/index.php">Новинки книг</a></li>
-                                        <li><a class="dropdown-item" href="/readers/event/index.php">Анонсы мероприятий</a></li>
-                                        <li><a class="dropdown-item" href="/readers/question/index.php">Вопрос-ответ</a></li>
-                                    </ul>
-                                </li>
-                                <li class="nav-item dropdown">
-                                    <a class="nav-link" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                        Ресурсы
-                                    </a>
-                                    <ul class="dropdown-menu">
-                                        <li><a class="dropdown-item" href="/resources/digital_catalogue/index.php">Электронный каталог</a></li>
-                                        <li><a class="dropdown-item" href="/resources/newspapers/index.php">Газеты</a></li>
-                                        <li><a class="dropdown-item" href="/resources/database/index.php">Базы данных</a></li>
-                                        <li><a class="dropdown-item" href="/resources/electronic_libraries/index.php">Электронные библиотеки</a></li>
-                                        <li><a class="dropdown-item" href="/resources/developments/index.php">Наши разработки</a></li>
-                                    </ul>
-                                </li>
-                                <li class="nav-item dropdown">
-                                    <a class="nav-link" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                        Краеведение
-                                    </a>
-                                    <ul class="dropdown-menu">
-                                        <li><a class="dropdown-item" href="/local_history/literary_map/index.php">Литературная карта</a></li>
-                                        <li><a class="dropdown-item" href="/local_history/chronicle/index.php">Летопись села</a></li>
-                                        <li><a class="dropdown-item" href="/local_history/books_of_memory/index.php">Книги памяти</a></li>
-                                        <li><a class="dropdown-item" href="/local_history/history_of_Motherland/index.php">История малой Родины в лицах</a></li>
-                                        <li><a class="dropdown-item" href="/local_history/dzhidinsky_district/index.php">Джидинский район от А до Я</a></li>
-                                    </ul>
-                                </li>
-                                <li class="nav-item dropdown">
-                                    <a class="nav-link" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                        События
-                                    </a>
-                                    <ul class="dropdown-menu">
-                                        <li><a class="dropdown-item" href="/events/pushkin_map/index.php">Пушкинская карта</a></li>
-                                        <li><a class="dropdown-item" href="/events/stock/index.php">Акции</a></li>
-                                        <li><a class="dropdown-item" href="/events/events/index.php">Мероприятия</a></li>
-                                    </ul>
-                                </li>
-                                <li class="nav-item dropdown">
-                                    <a class="nav-link" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                        О&nbspбиблиотеке
-                                    </a>
-                                    <ul class="dropdown-menu">
-                                        <li><a class="dropdown-item" href="/about/structure/index.php">Структура библиотеки</a></li>
-                                        <li><a class="dropdown-item" href="/about/docs/index.php">Документы</a></li>
-                                        <li><a class="dropdown-item" href="/about/operating_mode/index.php">Режим работы</a></li>
-                                        <li><a class="dropdown-item" href="/about/contacts/index.php">Контакты</a></li>
-                                        <li><a class="dropdown-item" href="/about/activity_report/index.php">Отчет о деятельности</a></li>
-                                        <li><a class="dropdown-item" href="/about/awards/index.php">Награды</a></li>
-                                        <li><a class="dropdown-item" href="/about/history/index.php">История</a></li>
-                                    </ul>
-                                </li>
-                            </ul>
-                            <form class="header__search visible-lg-block d-flex" role="search">
-                                <input type="text" name="q" value="<?=$_POST["q"];?>" placeholder="Поиск">
-                                <button name="SEND" value="Y"><i class="fa fa-search"></i></button>
-                            </form>
-                        </div>
+                <nav class="navigation navbar navbar-expand-lg fixed-top">
+                    <div class="logo py-3">
+                        <a href="/" class="d-flex align-items-center">
+                            <img src="/img/logo.png" alt="#" class="d-block me-3">
+                        </a>
+                    </div>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="header__menu-list collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li class="nav-item dropdown">
+                                <a class="nav-link" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Читателям
+                                </a>
+                                <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item" href="/readers/become_a_reader/index.php">Как стать читателем</a></li>
+                                    <li><a class="dropdown-item" href="/readers/services/index.php">Услуги</a></li>
+                                    <li><a class="dropdown-item" href="/readers/virtual_book/index.php">Виртуальные книжные выставки</a></li>
+                                    <li><a class="dropdown-item" href="/readers/new_books/index.php">Новинки книг</a></li>
+                                    <li><a class="dropdown-item" href="/readers/event/index.php">Анонсы мероприятий</a></li>
+                                    <li><a class="dropdown-item" href="/readers/question/index.php">Вопрос-ответ</a></li>
+                                </ul>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Ресурсы
+                                </a>
+                                <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item" href="/resources/digital_catalogue/index.php">Электронный каталог</a></li>
+                                    <li><a class="dropdown-item" href="/resources/newspapers/index.php">Газеты</a></li>
+                                    <li><a class="dropdown-item" href="/resources/database/index.php">Базы данных</a></li>
+                                    <li><a class="dropdown-item" href="/resources/electronic_libraries/index.php">Электронные библиотеки</a></li>
+                                    <li><a class="dropdown-item" href="/resources/developments/index.php">Наши разработки</a></li>
+                                </ul>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Краеведение
+                                </a>
+                                <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item" href="/local_history/literary_map/index.php">Литературная карта</a></li>
+                                    <li><a class="dropdown-item" href="/local_history/chronicle/index.php">Летопись села</a></li>
+                                    <li><a class="dropdown-item" href="/local_history/books_of_memory/index.php">Книги памяти</a></li>
+                                    <li><a class="dropdown-item" href="/local_history/history_of_Motherland/index.php">История малой Родины в лицах</a></li>
+                                    <li><a class="dropdown-item" href="/local_history/dzhidinsky_district/index.php">Джидинский район от А до Я</a></li>
+                                </ul>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    События
+                                </a>
+                                <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item" href="/events/pushkin_map/index.php">Пушкинская карта</a></li>
+                                    <li><a class="dropdown-item" href="/events/stock/index.php">Акции</a></li>
+                                    <li><a class="dropdown-item" href="/events/events/index.php">Мероприятия</a></li>
+                                </ul>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    О&nbspбиблиотеке
+                                </a>
+                                <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item" href="/about/structure/index.php">Структура библиотеки</a></li>
+                                    <li><a class="dropdown-item" href="/about/docs/index.php">Документы</a></li>
+                                    <li><a class="dropdown-item" href="/about/operating_mode/index.php">Режим работы</a></li>
+                                    <li><a class="dropdown-item" href="/about/contacts/index.php">Контакты</a></li>
+                                    <li><a class="dropdown-item" href="/about/activity_report/index.php">Отчет о деятельности</a></li>
+                                    <li><a class="dropdown-item" href="/about/awards/index.php">Награды</a></li>
+                                    <li><a class="dropdown-item" href="/about/history/index.php">История</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                        <form class="header__search visible-lg-block d-flex" role="search">
+                            <input type="text" name="q" value="<?=$_POST["q"];?>" placeholder="Поиск" class="form-control">
+                            <button name="SEND" value="Y"><i class="fa fa-search"></i></button>
+                        </form>
                     </div>
                 </nav>
             </div>
