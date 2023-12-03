1b0f200e8691137c3e6e0b992f81809203a9d2fa
winston138 <vadimfumioka@mail.ru>
2023-12-04 00:14:24 +0800

    создана news/detail event/detail

---
+++ events/events/detail.php
@@ -0,0 +1,155 @@
+<?require($_SERVER["DOCUMENT_ROOT"]."/local/templates/book/header.php");?>
+
+
+<div class="container">
+    <div class="wrap__event">
+        <div>
+
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb mt-5 mb-4">
+                    <li class="breadcrumb-item"><a href="/">Главная</a></li>
+                    <li class="breadcrumb-item"><a href="/">Мероприятия</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">Библионочь</li>
+                </ol>
+            </nav>
+
+            <div class="mt-3 mb-3">
+                <h2 class="mb-1">БИБЛИОНОЧЬ'23</h2>
+                <h2>13 сентября 2023г</h2>
+            </div>
+            <div class="d-flex justify-content-center mb-3">
+                <a href="/img/n2.png" data-fancybox="gallery" data-caption="Caption #1"><img src="/img/n2.png" /></a>
+            </div>
+            <div class="d-flex justify-content-between mb-5">
+                <a href="/img/n1.png" data-fancybox="gallery" data-caption="Caption #2"><img src="/img/n1.png" /></a>
+                <a href="/img/n1.png" data-fancybox="gallery" data-caption="Caption #3"><img src="/img/n1.png" /></a>
+                <a href="/img/n1.png" data-fancybox="gallery" data-caption="Caption #4"><img src="/img/n1.png" /></a>
+                <a href="/img/n1.png" data-fancybox="gallery" data-caption="Caption #5"><img src="/img/n1.png" /></a>
+            </div>
+            <div class="content__text">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. <br>
+                    A accusamus, aspernatur aut deleniti deserunt dignissimos expedita <br>
+                    incidunt minus nulla pariatur quis temporibus voluptas. Dolor ipsum <br>
+                    itaque perspiciatis qui quod sunt.
+
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. <br>
+                    A accusamus, aspernatur aut deleniti deserunt dignissimos expedita <br>
+                    incidunt minus nulla pariatur quis temporibus voluptas. Dolor ipsum <br>
+                    itaque perspiciatis qui quod sunt.
+
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. <br>
+                    A accusamus, aspernatur aut deleniti deserunt dignissimos expedita <br>
+                    incidunt minus nulla pariatur quis temporibus voluptas. Dolor ipsum <br>
+                    itaque perspiciatis qui quod sunt.
+
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. <br>
+                    A accusamus, aspernatur aut deleniti deserunt dignissimos expedita <br>
+                    incidunt minus nulla pariatur quis temporibus voluptas. Dolor ipsum <br>
+                    itaque perspiciatis qui quod sunt.
+
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. <br>
+                    A accusamus, aspernatur aut deleniti deserunt dignissimos expedita <br>
+                    incidunt minus nulla pariatur quis temporibus voluptas. Dolor ipsum <br>
+                    itaque perspiciatis qui quod sunt.
+
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. <br>
+                    A accusamus, aspernatur aut deleniti deserunt dignissimos expedita <br>
+                    incidunt minus nulla pariatur quis temporibus voluptas. Dolor ipsum <br>
+                    itaque perspiciatis qui quod sunt.
+
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. <br>
+                    A accusamus, aspernatur aut deleniti deserunt dignissimos expedita <br>
+                    incidunt minus nulla pariatur quis temporibus voluptas. Dolor ipsum <br>
+                    itaque perspiciatis qui quod sunt.
+
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. <br>
+                    A accusamus, aspernatur aut deleniti deserunt dignissimos expedita <br>
+                    incidunt minus nulla pariatur quis temporibus voluptas. Dolor ipsum <br>
+                    itaque perspiciatis qui quod sunt.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. <br>
+                    A accusamus, aspernatur aut deleniti deserunt dignissimos expedita <br>
+                    incidunt minus nulla pariatur quis temporibus voluptas. Dolor ipsum <br>
+                    itaque perspiciatis qui quod sunt.
+                </p>
+            </div>
+        </div>
+    </div>
+
+
+
+    <div class="footer__social d-flex justify-content-center">
+        <div class="wrap__social text-center">
+            <div class="create__line mt-5 mb-5"></div>
+                <div class="footer__social-text">ПОДЕЛИТЬСЯ</div>
+                <div class="footer__iconic d-flex justify-content-center">
+                    <div><a href=""><i class="fa-brands fa-vk"></i></a></div>
+                    <div><a href=""><i class="fa-brands fa-viber"></i></a></div>
+                    <div><a href=""><i class="fa-brands fa-whatsapp"></i></a></div>
+                </div>
+        </div>
+    </div>
+
+
+
+    <div class="main__news-section">
+        <div class="d-flex align-item-center mt-5">
+            <h2>СОБЫТИЯ</h2>
+            <a href="">смотреть всё</a>
+        </div>
+        <div class="d-flex gap-3">
+            <div class="main__news-block">
+                <div class="main__news__block-photo">
+                    <a href="">
+                        <img src="/img/n.png" alt="">
+                    </a>
+                </div>
+                <div class="main__news__block-title mt-2">
+                    <h2>ТАБАН БЭЛИГТЭН</h2>
+                </div>
+                <div class="main__news__block-description m-3">
+                    <p>Пока люд иговорят на родном языке <br> сохраняются народные ценности</p>
+                </div>
+                <div class="main__news__block-date m-3">
+                    <p>26 октября 2023</p>
+                </div>
+            </div>
+            <div class="main__news-block">
+                <div class="main__news__block-photo">
+                    <a href="">
+                        <img src="/img/n.png" alt="">
+                    </a>
+                </div>
+                <div class="main__news__block-title mt-2">
+                    <h2>ТАБАН БЭЛИГТЭН</h2>
+                </div>
+                <div class="main__news__block-description m-3">
+                    <p>Пока люд иговорят на родном языке <br> сохраняются народные ценности</p>
+                </div>
+                <div class="main__news__block-date m-3">
+                    <p>26 октября 2023</p>
+                </div>
+            </div>
+            <div class="main__news-block">
+                <div class="main__news__block-photo">
+                    <a href="">
+                        <img src="/img/n.png" alt="">
+                    </a>
+                </div>
+                <div class="main__news__block-title mt-2">
+                    <h2>ТАБАН БЭЛИГТЭН</h2>
+                </div>
+                <div class="main__news__block-description m-3">
+                    <p>Пока люд иговорят на родном языке <br> сохраняются народные ценности</p>
+                </div>
+                <div class="main__news__block-date m-3">
+                    <p>26 октября 2023</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+<?require($_SERVER["DOCUMENT_ROOT"]."/local/templates/book/footer.php");?>
--- news/index.php
+++ news/index.php
@@ -1,54 +1,16 @@
 <?require($_SERVER["DOCUMENT_ROOT"]."/local/templates/book/header.php");?>
 
-
 <div class="container">
-    <div class="main">
-        <div class="main__bg rounded-3">
-            <div class="main__text">
-                По своему типу краеведение ближе к истории географии.
-                Но в отличие от последней, большре значение придает
-                отдельным замечательным людям, истории науки,
-                литературы и искусства, в частности, архитектуры.
-                В этом замечательная особенность и сила краеведения.
-                <br>
-                -Д.С. Лихачев
-            </div>
-
-            <div class="main__title">
-                <div class="main__title-item">
-                    <div class="main__title-text">КРАЕВЕДЕНИЕ</div>
-                    <div class="main__tiitle-link"><a href="/">подробно</a></div>
-                </div>
-            </div>
-
-            <div class="main__chapter">
-	            <a href="/local_history/literary_map/" class="main__chapter-link d-block p-3 fw-bold">
-		            ЛИТЕРАТУРНАЯ КАРТА
-	            </a>
-	            <a href="/local_history/chronicle/" class="main__chapter-link d-block p-3 fw-bold">
-		            ЛЕТОПИСЬ СЕЛА
-	            </a>
-	            <a href="/local_history/books_of_memory/" class="main__chapter-link d-block p-3 fw-bold">
-		            КНИГА ПАМЯТИ
-	            </a>
-	            <a href="/local_history/history_of_Motherland/" class="main__chapter-link d-block p-3 fw-bold">
-		            ИСТОРИЯ В ЛИЦАХ
-	            </a>
-	            <a href="/local_history/dzhidinsky_district/" class="main__chapter-link d-block p-3 fw-bold">
-		            ДЖИДИНСКИЙ РАЙОН
-	            </a>
-            </div>
-        </div>
-
+    <div class="main__news">
         <div class="main__news-section">
             <div class="d-flex align-item-center mt-5">
                 <h2 class="m-1-1-2-2">НОВОСТИ</h2>
-                <a href="/news/">смотреть всё</a>
+<!--                <a href="/news/index.php">смотреть всё</a>-->
             </div>
             <div class="d-flex gap-3">
                 <div class="main__news-block">
                     <div class="main__news__block-photo">
-                        <a href="">
+                        <a href="/news/detail.php">
                             <img src="/img/n.png" alt="">
                         </a>
                     </div>
@@ -64,7 +26,7 @@
                 </div>
                 <div class="main__news-block">
                     <div class="main__news__block-photo">
-                        <a href="">
+                        <a href="/news/detail.php">
                             <img src="/img/n.png" alt="">
                         </a>
                     </div>
@@ -80,7 +42,7 @@
                 </div>
                 <div class="main__news-block">
                     <div class="main__news__block-photo">
-                        <a href="">
+                        <a href="/news/detail.php">
                             <img src="/img/n.png" alt="">
                         </a>
                     </div>
@@ -96,17 +58,15 @@
                 </div>
             </div>
         </div>
-
-
         <div class="main__news-section">
             <div class="d-flex align-item-center mt-5">
-                <h2>МЕРОПРИЯТИЯ</h2>
-                <a href="/events/events/">смотреть всё</a>
+<!--                <h2 class="m-1-1-2-2">НОВОСТИ</h2>-->
+<!--                <a href="/news/index.php">смотреть всё</a>-->
             </div>
             <div class="d-flex gap-3">
                 <div class="main__news-block">
                     <div class="main__news__block-photo">
-                        <a href="">
+                        <a href="/news/detail.php">
                             <img src="/img/n.png" alt="">
                         </a>
                     </div>
@@ -122,7 +82,7 @@
                 </div>
                 <div class="main__news-block">
                     <div class="main__news__block-photo">
-                        <a href="">
+                        <a href="/news/detail.php">
                             <img src="/img/n.png" alt="">
                         </a>
                     </div>
@@ -138,7 +98,7 @@
                 </div>
                 <div class="main__news-block">
                     <div class="main__news__block-photo">
-                        <a href="">
+                        <a href="/news/detail.php">
                             <img src="/img/n.png" alt="">
                         </a>
                     </div>
@@ -154,17 +114,64 @@
                 </div>
             </div>
         </div>
-
-
+        <div class="main__news-section">
+            <div class="d-flex align-item-center mt-5">
+<!--                <h2 class="m-1-1-2-2">НОВОСТИ</h2>-->
+<!--                <a href="/news/index.php">смотреть всё</a>-->
+            </div>
+            <div class="d-flex gap-3">
+                <div class="main__news-block">
+                    <div class="main__news__block-photo">
+                        <a href="/news/detail.php">
+                            <img src="/img/n.png" alt="">
+                        </a>
+                    </div>
+                    <div class="main__news__block-title mt-2">
+                        <h2>ТАБАН БЭЛИГТЭН</h2>
+                    </div>
+                    <div class="main__news__block-description m-3">
+                        <p>Пока люд иговорят на родном языке <br> сохраняются народные ценности</p>
+                    </div>
+                    <div class="main__news__block-date m-3">
+                        <p>26 октября 2023</p>
+                    </div>
+                </div>
+                <div class="main__news-block">
+                    <div class="main__news__block-photo">
+                        <a href="/news/detail.php">
+                            <img src="/img/n.png" alt="">
+                        </a>
+                    </div>
+                    <div class="main__news__block-title mt-2">
+                        <h2>ТАБАН БЭЛИГТЭН</h2>
+                    </div>
+                    <div class="main__news__block-description m-3">
+                        <p>Пока люд иговорят на родном языке <br> сохраняются народные ценности</p>
+                    </div>
+                    <div class="main__news__block-date m-3">
+                        <p>26 октября 2023</p>
+                    </div>
+                </div>
+                <div class="main__news-block">
+                    <div class="main__news__block-photo">
+                        <a href="/news/detail.php">
+                            <img src="/img/n.png" alt="">
+                        </a>
+                    </div>
+                    <div class="main__news__block-title mt-2">
+                        <h2>ТАБАН БЭЛИГТЭН</h2>
+                    </div>
+                    <div class="main__news__block-description m-3">
+                        <p>Пока люд иговорят на родном языке <br> сохраняются народные ценности</p>
+                    </div>
+                    <div class="main__news__block-date m-3">
+                        <p>26 октября 2023</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
-
-
-
-
-
-
-
 
 
 
